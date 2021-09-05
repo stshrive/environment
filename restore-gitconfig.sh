558086a5b8302352087761adf3b0a179e89c7182
Steven Shriver <6007067+stshrive@users.noreply.github.com>
2021-09-05 13:03:36 -0700

    Add movebranch command

--- restore-gitconfig.sh
+++ restore-gitconfig.sh
@@ -9,6 +9,9 @@ cat << EOF > ~/.gitconfig
 [alias]
     graph = log --graph --abbrev-commit --decorate --all \
         --format='%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold yellow)%d%C(reset)%n''          %C(white)%s%C(reset)'
+    movebranch = "!f() { \
+            git update-ref -m \"reset: Reset ${1} to ${2}\" refs/heads/${1} ${2}; \
+        }; f"
 [core]
         editor = vim
         autoclrf = true
